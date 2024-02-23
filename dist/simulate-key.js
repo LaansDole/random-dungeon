9b7faf8cb3014b9b79d4b53d54ca8f5655cba890
dolelongan <dolelongan@gmail.com>
2024-02-23 15:10:28 +0700

    feature: WASD buttons for mobile

--- dist/simulate-key.js
+++ dist/simulate-key.js
@@ -10,3 +10,30 @@ function simulateKeyPress(keyCode) {
     document.body.dispatchEvent(event);
 }
 
+// Mapping of keys to button ids
+const keyMap = {
+    '87': 'up',    // 'W' key
+    '65': 'left',  // 'A' key
+    '83': 'down',  // 'S' key
+    '68': 'right', // 'D' key
+    '49': 'heal'   // '1' key
+};
+
+// Add the 'highlight' class to the button when the key is pressed
+document.addEventListener('keydown', function (event) {
+    const buttonId = keyMap[event.keyCode.toString()];
+    if (buttonId) {
+        const button = document.getElementById(buttonId);
+        button.classList.add('btn-outline-warning');
+    }
+});
+
+// Remove the 'highlight' class when the key is released
+document.addEventListener('keyup', function (event) {
+    const buttonId = keyMap[event.keyCode.toString()];
+    if (buttonId) {
+        const button = document.getElementById(buttonId);
+        button.classList.remove('btn-outline-warning');
+    }
+});
+
